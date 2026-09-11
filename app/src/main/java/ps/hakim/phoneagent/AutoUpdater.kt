475fab0de6ae8a0cdb31046de2c2b0f0ac33f599
smileeyes1 <smile.eyes1@gmail.com>
2026-09-11 21:51:32 +0300

    حكيم: إصلاح استيراد جدولة التحديث

--- app/src/main/java/ps/hakim/phoneagent/AutoUpdater.kt
+++ app/src/main/java/ps/hakim/phoneagent/AutoUpdater.kt
@@ -1,11 +1,11 @@
 package ps.hakim.phoneagent
 
-import android.app.JobInfo
-import android.app.JobScheduler
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent

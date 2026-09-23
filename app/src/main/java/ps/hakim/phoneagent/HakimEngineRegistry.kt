b6324ff2f8fefc2a4bc0eb25df831d0926bb04da
smileeyes1 <smile.eyes1@gmail.com>
2026-09-23 19:39:51 +0300

    router: prefer linked OpenRouter free direct engine

--- app/src/main/java/ps/hakim/phoneagent/HakimEngineRegistry.kt
+++ app/src/main/java/ps/hakim/phoneagent/HakimEngineRegistry.kt
@@ -11,6 +11,9 @@ import android.content.Context
 object HakimEngineRegistry {
     fun directEngines(context: Context): List<HakimInferenceEngine> {
         val out = mutableListOf<HakimInferenceEngine>()
+        if (HakimSecretStore.has(context, OpenRouterPkceAuth.SECRET_OPENROUTER_KEY)) {
+            out += OpenRouterDirectEngine(context)
+        }
         if (HakimSecretStore.has(context, GeminiDirectEngine.SECRET_GEMINI_KEY)) {
             out += GeminiDirectEngine(context)
         }

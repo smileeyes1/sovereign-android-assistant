7b798ebeeb0076c39b5c88f23dba9f1f8ab69762
smileeyes1 <smile.eyes1@gmail.com>
2026-09-24 05:42:33 +0300

    حكيم 20110: استعادة رصد حارس الشبكة ونسيج التنفيذ في القناة

--- app/src/main/java/ps/hakim/phoneagent/HakimUnifiedRelay.kt
+++ app/src/main/java/ps/hakim/phoneagent/HakimUnifiedRelay.kt
@@ -334,6 +334,8 @@ object HakimUnifiedRelay {
             .put("accessibility", HakimAccessibilityService.instance != null)
             .put("notification_listener", HakimNotificationListener.isConnected())
             .put("auto_update", AutoUpdater.diagnostics(context))
+            .put("network_guardian", HakimNetworkGuardian.status(context))
+            .put("execution_fabric", HakimExecutionFabric.status(context))
             .put("self_check", self.getString("last_self_check_status", "NOT_TESTED"))
             .put("learning", HakimLearning.snapshot(context))
     }

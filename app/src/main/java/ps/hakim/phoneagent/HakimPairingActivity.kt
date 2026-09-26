a724a2e76e4b3fdb4c2c9500a4e9ee921be12801
smileeyes1 <smile.eyes1@gmail.com>
2026-09-26 14:16:06 +0300

    Hakim Android: pair direct bridge origin with secure topics

--- app/src/main/java/ps/hakim/phoneagent/HakimPairingActivity.kt
+++ app/src/main/java/ps/hakim/phoneagent/HakimPairingActivity.kt
@@ -23,12 +23,13 @@ class HakimPairingActivity : Activity() {
         val uri = i?.data
         val token = uri?.getQueryParameter("token").orEmpty()
         val topic = uri?.getQueryParameter("relay_topic")
-        val resultUrl = uri?.getQueryParameter("result_url")
+        val resultTopic = uri?.getQueryParameter("relay_result_topic")
         val relayKey = uri?.getQueryParameter("relay_key")
+        val bridgeBase = uri?.getQueryParameter("bridge_base")
 
         val ok = uri?.scheme == "hakim" && uri.host == "pair" &&
             token.length in 32..256 &&
-            HakimUnifiedRelay.configure(this, topic, resultUrl, relayKey)
+            HakimUnifiedRelay.configure(this, topic, resultTopic, relayKey, bridgeBase)
 
         if (ok) {
             getSharedPreferences("hakim", MODE_PRIVATE).edit()
